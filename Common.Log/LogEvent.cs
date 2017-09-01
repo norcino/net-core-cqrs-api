2a3d3c91c90d36b7e2e0919b9919f23a45258eca
Manuel Salvatori <manuel.salvatori@gmail.com>
2017-09-01 21:01:35 +0100

    Added missing log project

---
+++ Common.Log/LogEvent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Common.Log
+{
+    public static class LogEvent
+    {
+        static LogEvent()
+        {
+            QueryHandlying = new EventId(1, "QueryHandlying");
+            CommandHandlying = new EventId(2, "QueryHandlying");
+        }
+
+        public static EventId QueryHandlying { get; }
+        public static EventId CommandHandlying { get; }
+    }
+}
